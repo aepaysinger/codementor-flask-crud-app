49229a262ee29ddfc34dc74f6d2c829505b37f56
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-03-14 13:47:51 -0500

    ran black

--- flask-crud-app/bookmanager.py
+++ flask-crud-app/bookmanager.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlite3 import IntegrityError
 
 
-
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
 
@@ -14,6 +13,7 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db = SQLAlchemy(app)
+
 
 class Book(db.Model):
     title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
@@ -37,6 +37,7 @@ def home():
     books = Book.query.all()
     return render_template("home.html", books=books)
 
+
 @app.route("/update", methods=["POST"])
 def update():
     try:
@@ -50,6 +51,7 @@ def update():
         print(e)
     return redirect("/")
 
+
 @app.route("/delete", methods=["POST"])
 def delete():
     title = request.form.get("title")
@@ -59,6 +61,5 @@ def delete():
     return redirect("/")
 
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", debug=True)
